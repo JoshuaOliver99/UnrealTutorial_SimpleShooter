c4d9e4d65cd09fa0335bb69cd7cbf79244dba8f2
JoshuaOliver99 <joshuaoliversimpson@gmail.com>
2023-02-05 15:37:27 +0000

    Completed "Checking AI Line Of Sight"

---
+++ SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterAIController.h"
+
+#include "Kismet/GameplayStatics.h"
+
+void AShooterAIController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	
+	APawn* PlayerPawn =	UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	if (LineOfSightTo(PlayerPawn))
+	{
+		MoveToActor(PlayerPawn, AcceptanceRadius);
+		SetFocus(PlayerPawn);
+	}
+	else
+	{
+		ClearFocus(EAIFocusPriority::Gameplay);
+		StopMovement();
+	}
+}
+
+void AShooterAIController::BeginPlay()
+{
+	Super::BeginPlay();
+ }
